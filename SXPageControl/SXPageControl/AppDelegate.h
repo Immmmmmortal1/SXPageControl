9ca4efcda82a5cfd28ea85ddcd22c0611dc04ac4
shuxia <1347715389@qq.com>
2021-02-05 14:20:52 +0800

    第二次提交

---
+++ SXPageControl/SXPageControl/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  SXPageControl
+//
+//  Created by 树下 on 2020/12/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ SXPageControl/SXPageControl/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  SXPageControl
+//
+//  Created by 树下 on 2020/12/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
